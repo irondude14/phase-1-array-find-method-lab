7ca81bab92b78ca5051ce5bd25f7c54ef1cd11ac
Vladyslav <vkadyrov@protonmail.com>
2022-10-04 17:28:25 -0400

    commit tree bbed8fd20e8b3155fa8c60619667b734ee339d49

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function superbowlWin(record) {
+    // for (const obj of record) {
+    //     if (obj.result === "W") {
+    //         return obj.year;
+    //     }; 
+    // };
+    let winningRecord = record.find(obj => obj.result === "W")
+    return !!winningRecord ? winningRecord.year : undefined;
+};
+ 
+
+
+
+
